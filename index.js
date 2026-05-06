8c5aa9d6957ef54966e37c7e6eca307373e569b2
popesmoke <121421246+popesmoke@users.noreply.github.com>
2026-05-06 22:53:37 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const Eris = require("eris");
+const keep_alive = require("./keep_alive.js");
+
+if (!process.env.token) {
+  console.warn(
+    "WARNING: No 'token' secret found. Set it in Replit Secrets to connect to Discord.",
+  );
+} else {
+  const bot = new Eris(process.env.token);
+
+  bot.on("error", (err) => {
+    console.error(err);
+  });
+
+  bot.connect();
+}
